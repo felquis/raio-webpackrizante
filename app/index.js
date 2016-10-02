3865bf37f3e3c27f46a2b627999ca841e92bb0ce
Felquis Gimenes <frgformenton@gmail.com>
2016-10-02 07:53:39 -0300

    Use Array.from, map from core-js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
-require('./styles.css')
+import './styles.css'
+import From from '../node_modules/core-js/library/fn/array/from'
+import map from '../node_modules/core-js/library/fn/array/map'
 
 const look = require('./look')
 
 document.body.insertAdjacentHTML('afterbegin', look())
+
+var a = map( From(document.querySelectorAll('*')), (element, index) => {
+	element.setAttribute(`data-id`, index)
+	return element
+})
